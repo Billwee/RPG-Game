64ebdb445d4f3c3293d6f86461828780b372fbcb
Bill Kamberos <bill.kamberos@gmail.com>
2020-01-20 01:13:57 -0500

    Other character selections complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,26 @@
 var baby = {
-  hp: 180,
+  hp: 170,
   ap: 15,
   baseAp: 15,
-  cap: 25
+  cap: 20
 };
 
 var mando = {
   hp: 150,
-  ap: 15,
-  baseAp: 15,
+  ap: 20,
+  baseAp: 20,
   cap: 25
 };
 var droid = {
   hp: 110,
   ap: 15,
   baseAp: 15,
-  cap: 25
+  cap: 20
 };
 var kuiil = {
   hp: 120,
-  ap: 15,
-  baseAp: 15,
+  ap: 25,
+  baseAp: 25,
   cap: 15
 };
 
@@ -92,7 +92,6 @@ $('.babyDiv').click(function() {
     selectFighter(droidChar, mandoChar, kuiilChar);
     fighter = true;
   }
-
   $('.mandoDiv').click(function() {
     if (fighter) {
       $('.mandoDiv').remove();
@@ -145,6 +144,9 @@ $('.babyDiv').click(function() {
         baby.ap += baby.baseAp;
         healthDisp();
         if (baby.hp < 1) {
+          if (kuiil.hp < 1) {
+            kuiil.hp = 0;
+          }
           baby.hp = 0;
           healthDisp();
           $('.popup1').text('You died. Game Over');
@@ -170,6 +172,9 @@ $('.babyDiv').click(function() {
         baby.ap += baby.baseAp;
         healthDisp();
         if (baby.hp < 1) {
+          if (mando.hp < 1) {
+            mando.hp = 0;
+          }
           baby.hp = 0;
           healthDisp();
           $('.popup1').text('You died. Game Over');
@@ -190,11 +195,14 @@ $('.babyDiv').click(function() {
       if (yourOpponent === droid) {
         baby.hp -= droid.cap;
         droid.hp -= baby.ap;
-        $('.popup1').text('Droid IG-11 attacks for ' + baby.ap);
+        $('.popup1').text('Baby Yoda attacks for ' + baby.ap);
         $('.popup2').text('Droid IG-11 counter attacks for ' + droid.cap);
         baby.ap += baby.baseAp;
         healthDisp();
         if (baby.hp < 1) {
+          if (droid.hp < 1) {
+            droid.hp = 0;
+          }
           baby.hp = 0;
           healthDisp();
           $('.popup1').text('You died. Game Over');
@@ -221,6 +229,10 @@ $('.babyDiv').click(function() {
     }
   });
 }); //End of click
+//
+//
+//
+//
 
 $('.mandoDiv').click(function() {
   if (!fighterSelected) {
@@ -228,80 +240,431 @@ $('.mandoDiv').click(function() {
     selectFighter(droidChar, babyChar, kuiilChar);
     fighter = this;
   }
-  // if (fighter === this) {
-  //   $('.babyDiv').click(function() {
-  //     $('.babyDiv').remove();
-  //     $('.selectOpponent').css('display', 'none');
-  //     $('.selectedOpponent').html(babyChar);
-  //   });
+  $('.babyDiv').click(function() {
+    if (fighter) {
+      $('.babyDiv').remove();
+      $('.selectOpponent').css('display', 'none');
+      $('.selectedOpponent').html(babyChar);
+      healthDisp();
+      $('.popup1').text('');
+      $('.popup2').text('');
+    }
+    fighter = false;
+    attack = true;
+    yourOpponent = baby;
+  });
 
-  //   $('.droidDiv').click(function() {
-  //     $('.droidDiv').remove();
-  //     $('.selectOpponent').css('display', 'none');
-  //     $('.selectedOpponent').html(droidChar);
-  //   });
+  $('.droidDiv').click(function() {
+    if (fighter) {
+      $('.droidDiv').remove();
+      $('.selectOpponent').css('display', 'none');
+      $('.selectedOpponent').html(droidChar);
+      healthDisp();
+      $('.popup1').text('');
+      $('.popup2').text('');
+    }
+    fighter = false;
+    attack = true;
+    yourOpponent = droid;
+  });
 
-  //   $('.kuiilDiv').click(function() {
-  //     $('.kuiilDiv').remove();
-  //     $('.selectOpponent').css('display', 'none');
-  //     $('.selectedOpponent').html(droidChar);
-  //   });
-  // }
+  $('.kuiilDiv').click(function() {
+    if (fighter) {
+      $('.kuiilDiv').remove();
+      $('.selectOpponent').css('display', 'none');
+      $('.selectedOpponent').html(kuiilChar);
+      healthDisp();
+      $('.popup1').text('');
+      $('.popup2').text('');
+    }
+    fighter = false;
+    attack = true;
+    yourOpponent = kuiil;
+  });
+
+  $('.attackBtn').click(function() {
+    if (attack) {
+      if (yourOpponent === kuiil) {
+        mando.hp -= kuiil.cap;
+        kuiil.hp -= mando.ap;
+        $('.popup1').text('The Mandalorian attacks for ' + mando.ap);
+        $('.popup2').text('Kuiil counter attacks for ' + kuiil.cap);
+        mando.ap += mando.baseAp;
+        healthDisp();
+        if (mando.hp < 1) {
+          if (kuiil.hp < 1) {
+            kuiil.hp = 0;
+          }
+          mando.hp = 0;
+          healthDisp();
+          $('.popup1').text('You died. Game Over');
+          $('.popup2').text('Press button to retry');
+          $('.attackBtn').css('display', 'none');
+          $('.restart').css('display', 'block');
+        } else if (kuiil.hp < 1) {
+          kuiil.hp = 0;
+          healthDisp();
+          $('.popup1').text('Kuiil has been defeated!');
+          $('.popup2').text('Select another opponent.');
+          wins++;
+          attack = false;
+          fighter = true;
+        }
+        winner(wins);
+      }
+      if (yourOpponent === baby) {
+        mando.hp -= baby.cap;
+        baby.hp -= mando.ap;
+        $('.popup1').text('The Mandalorian attacks for ' + mando.ap);
+        $('.popup2').text('Baby Yoda counter attacks for ' + baby.cap);
+        mando.ap += mando.baseAp;
+        healthDisp();
+        if (mando.hp < 1) {
+          mando.hp = 0;
+          healthDisp();
+          $('.popup1').text('You died. Game Over');
+          $('.popup2').text('Press button to retry');
+          $('.attackBtn').css('display', 'none');
+          $('.restart').css('display', 'block');
+        } else if (baby.hp < 1) {
+          baby.hp = 0;
+          healthDisp();
+          $('.popup1').text('Baby Yoda has been defeated!');
+          $('.popup2').text('Select another opponent.');
+          wins++;
+          attack = false;
+          fighter = true;
+        }
+        winner(wins);
+      }
+      if (yourOpponent === droid) {
+        mando.hp -= droid.cap;
+        droid.hp -= mando.ap;
+        $('.popup1').text('The Mandalorian attacks for ' + mando.ap);
+        $('.popup2').text('Droid IG-11 counter attacks for ' + droid.cap);
+        mando.ap += mando.baseAp;
+        healthDisp();
+        if (mando.hp < 1) {
+          if (droid.hp < 1) {
+            droid.hp = 0;
+          }
+          mando.hp = 0;
+          healthDisp();
+          $('.popup1').text('You died. Game Over');
+          $('.popup2').text('Press button to retry');
+          $('.attackBtn').css('display', 'none');
+          $('.restart').css('display', 'block');
+        } else if (droid.hp < 1) {
+          droid.hp = 0;
+          healthDisp();
+          $('.popup1').text('Droid IG-11 has been defeated!');
+          $('.popup2').text('Select another opponent.');
+          wins++;
+          attack = false;
+          fighter = true;
+        }
+        winner(wins);
+      }
+    } else {
+      $('.popup1').text('Please Select Opponent!');
+      $('.popup2').text('');
+      setTimeout(function() {
+        $('.popup1').text('');
+      }, 1500);
+    }
+  });
 }); //End of click
+//
+//
+//
+//
 
 $('.droidDiv').click(function() {
   if (!fighterSelected) {
     $('.fighter').html(this);
     selectFighter(babyChar, mandoChar, kuiilChar);
-    fighter = this;
+    fighter = true;
   }
-  // if (fighter === this) {
-  //   $('.babyDiv').click(function() {
-  //     $('.babyDiv').remove();
-  //     $('.selectOpponent').css('display', 'none');
-  //     $('.selectedOpponent').html(babyChar);
-  //   });
+  $('.mandoDiv').click(function() {
+    if (fighter) {
+      $('.mandoDiv').remove();
+      $('.selectOpponent').css('display', 'none');
+      $('.selectedOpponent').html(mandoChar);
+      healthDisp();
+      $('.popup1').text('');
+      $('.popup2').text('');
+    }
+    fighter = false;
+    attack = true;
+    yourOpponent = mando;
+  });
 
-  //   $('.mandoDiv').click(function() {
-  //     $('.mandoDiv').remove();
-  //     $('.selectOpponent').css('display', 'none');
-  //     $('.selectedOpponent').html(mandoChar);
-  //   });
+  $('.babyDiv').click(function() {
+    if (fighter) {
+      $('.babyDiv').remove();
+      $('.selectOpponent').css('display', 'none');
+      $('.selectedOpponent').html(babyChar);
+      healthDisp();
+      $('.popup1').text('');
+      $('.popup2').text('');
+    }
+    fighter = false;
+    attack = true;
+    yourOpponent = baby;
+  });
 
-  //   $('.kuiilDiv').click(function() {
-  //     $('.kuiilDiv').remove();
-  //     $('.selectOpponent').css('display', 'none');
-  //     $('.selectedOpponent').html(droidChar);
-  //   });
-  // }
+  $('.kuiilDiv').click(function() {
+    if (fighter) {
+      $('.kuiilDiv').remove();
+      $('.selectOpponent').css('display', 'none');
+      $('.selectedOpponent').html(kuiilChar);
+      healthDisp();
+      $('.popup1').text('');
+      $('.popup2').text('');
+    }
+    fighter = false;
+    attack = true;
+    yourOpponent = kuiil;
+  });
+
+  $('.attackBtn').click(function() {
+    if (attack) {
+      if (yourOpponent === kuiil) {
+        baby.hp -= kuiil.cap;
+        kuiil.hp -= baby.ap;
+        $('.popup1').text('Baby Yoda attacks for ' + baby.ap);
+        $('.popup2').text('Kuiil counter attacks for ' + kuiil.cap);
+        baby.ap += baby.baseAp;
+        healthDisp();
+        if (baby.hp < 1) {
+          if (kuiil.hp < 1) {
+            kuiil.hp = 0;
+          }
+          baby.hp = 0;
+          healthDisp();
+          $('.popup1').text('You died. Game Over');
+          $('.popup2').text('Press button to retry');
+          $('.attackBtn').css('display', 'none');
+          $('.restart').css('display', 'block');
+        } else if (kuiil.hp < 1) {
+          kuiil.hp = 0;
+          healthDisp();
+          $('.popup1').text('Kuiil has been defeated!');
+          $('.popup2').text('Select another opponent.');
+          wins++;
+          attack = false;
+          fighter = true;
+        }
+        winner(wins);
+      }
+      if (yourOpponent === mando) {
+        baby.hp -= mando.cap;
+        mando.hp -= baby.ap;
+        $('.popup1').text('Baby Yoda attacks for ' + baby.ap);
+        $('.popup2').text('The Mandalorian counter attacks for ' + mando.cap);
+        baby.ap += baby.baseAp;
+        healthDisp();
+        if (baby.hp < 1) {
+          if (mando.hp < 1) {
+            mando.hp = 0;
+          }
+          baby.hp = 0;
+          healthDisp();
+          $('.popup1').text('You died. Game Over');
+          $('.popup2').text('Press button to retry');
+          $('.attackBtn').css('display', 'none');
+          $('.restart').css('display', 'block');
+        } else if (mando.hp < 1) {
+          mando.hp = 0;
+          healthDisp();
+          $('.popup1').text('The Mandalorian has been defeated!');
+          $('.popup2').text('Select another opponent.');
+          wins++;
+          attack = false;
+          fighter = true;
+        }
+        winner(wins);
+      }
+      if (yourOpponent === baby) {
+        droid.hp -= baby.cap;
+        baby.hp -= droid.ap;
+        $('.popup1').text('Droid IG-11 attacks for ' + droid.ap);
+        $('.popup2').text('Baby Yoda counter attacks for ' + baby.cap);
+        droid.ap += droid.baseAp;
+        healthDisp();
+        if (droid.hp < 1) {
+          if (baby.hp < 1) {
+            baby.hp = 0;
+          }
+          droid.hp = 0;
+          healthDisp();
+          $('.popup1').text('You died. Game Over');
+          $('.popup2').text('Press button to retry');
+          $('.attackBtn').css('display', 'none');
+          $('.restart').css('display', 'block');
+        } else if (baby.hp < 1) {
+          baby.hp = 0;
+          healthDisp();
+          $('.popup1').text('Baby Yoda has been defeated!');
+          $('.popup2').text('Select another opponent.');
+          wins++;
+          attack = false;
+          fighter = true;
+        }
+        winner(wins);
+      }
+    } else {
+      $('.popup1').text('Please Select Opponent!');
+      $('.popup2').text('');
+      setTimeout(function() {
+        $('.popup1').text('');
+      }, 1500);
+    }
+  });
 }); //End of click
 
 $('.kuiilDiv').click(function() {
   if (!fighterSelected) {
     $('.fighter').html(this);
     selectFighter(droidChar, mandoChar, babyChar);
-    fighter = this;
+    fighter = true;
   }
+  $('.mandoDiv').click(function() {
+    if (fighter) {
+      $('.mandoDiv').remove();
+      $('.selectOpponent').css('display', 'none');
+      $('.selectedOpponent').html(mandoChar);
+      healthDisp();
+      $('.popup1').text('');
+      $('.popup2').text('');
+    }
+    fighter = false;
+    attack = true;
+    yourOpponent = mando;
+  });
 
-  // if (fighter === this) {
-  //   $('.babyDiv').click(function() {
-  //     $('.babyDiv').remove();
-  //     $('.selectOpponent').css('display', 'none');
-  //     $('.selectedOpponent').html(babyChar);
-  //   });
+  $('.droidDiv').click(function() {
+    if (fighter) {
+      $('.droidDiv').remove();
+      $('.selectOpponent').css('display', 'none');
+      $('.selectedOpponent').html(droidChar);
+      healthDisp();
+      $('.popup1').text('');
+      $('.popup2').text('');
+    }
+    fighter = false;
+    attack = true;
+    yourOpponent = droid;
+  });
 
-  //   $('.mandoDiv').click(function() {
-  //     $('.mandoDiv').remove();
-  //     $('.selectOpponent').css('display', 'none');
-  //     $('.selectedOpponent').html(mandoChar);
-  //   });
+  $('.babyDiv').click(function() {
+    if (fighter) {
+      $('.babyDiv').remove();
+      $('.selectOpponent').css('display', 'none');
+      $('.selectedOpponent').html(babyChar);
+      healthDisp();
+      $('.popup1').text('');
+      $('.popup2').text('');
+    }
+    fighter = false;
+    attack = true;
+    yourOpponent = baby;
+  });
 
-  //   $('.droidDiv').click(function() {
-  //     $('.droidDiv').remove();
-  //     $('.selectOpponent').css('display', 'none');
-  //     $('.selectedOpponent').html(droidChar);
-  //   });
-  // }
+  $('.attackBtn').click(function() {
+    if (attack) {
+      if (yourOpponent === baby) {
+        kuiil.hp -= baby.cap;
+        baby.hp -= kuiil.ap;
+        $('.popup1').text('Kuiil attacks for ' + kuiil.ap);
+        $('.popup2').text('Baby Yoda counter attacks for ' + baby.cap);
+        kuiil.ap += kuiil.baseAp;
+        healthDisp();
+        if (kuiil.hp < 1) {
+          if (baby.hp < 1) {
+            baby.hp = 0;
+          }
+          kuiil.hp = 0;
+          healthDisp();
+          $('.popup1').text('You died. Game Over');
+          $('.popup2').text('Press button to retry');
+          $('.attackBtn').css('display', 'none');
+          $('.restart').css('display', 'block');
+        } else if (baby.hp < 1) {
+          baby.hp = 0;
+          healthDisp();
+          $('.popup1').text('Baby Yoda has been defeated!');
+          $('.popup2').text('Select another opponent.');
+          wins++;
+          attack = false;
+          fighter = true;
+        }
+        winner(wins);
+      }
+      if (yourOpponent === mando) {
+        baby.hp -= mando.cap;
+        mando.hp -= baby.ap;
+        $('.popup1').text('Baby Yoda attacks for ' + baby.ap);
+        $('.popup2').text('The Mandalorian counter attacks for ' + mando.cap);
+        baby.ap += baby.baseAp;
+        healthDisp();
+        if (baby.hp < 1) {
+          if (mando.hp < 1) {
+            mando.hp = 0;
+          }
+          baby.hp = 0;
+          healthDisp();
+          $('.popup1').text('You died. Game Over');
+          $('.popup2').text('Press button to retry');
+          $('.attackBtn').css('display', 'none');
+          $('.restart').css('display', 'block');
+        } else if (mando.hp < 1) {
+          mando.hp = 0;
+          healthDisp();
+          $('.popup1').text('The Mandalorian has been defeated!');
+          $('.popup2').text('Select another opponent.');
+          wins++;
+          attack = false;
+          fighter = true;
+        }
+        winner(wins);
+      }
+      if (yourOpponent === droid) {
+        baby.hp -= droid.cap;
+        droid.hp -= baby.ap;
+        $('.popup1').text('Baby Yoda attacks for ' + baby.ap);
+        $('.popup2').text('Droid IG-11 counter attacks for ' + droid.cap);
+        baby.ap += baby.baseAp;
+        healthDisp();
+        if (baby.hp < 1) {
+          if (droid.hp < 1) {
+            droid.hp = 0;
+          }
+          baby.hp = 0;
+          healthDisp();
+          $('.popup1').text('You died. Game Over');
+          $('.popup2').text('Press button to retry');
+          $('.attackBtn').css('display', 'none');
+          $('.restart').css('display', 'block');
+        } else if (droid.hp < 1) {
+          droid.hp = 0;
+          healthDisp();
+          $('.popup1').text('Droid IG-11 has been defeated!');
+          $('.popup2').text('Select another opponent.');
+          wins++;
+          attack = false;
+          fighter = true;
+        }
+        winner(wins);
+      }
+    } else {
+      $('.popup1').text('Please Select Opponent!');
+      $('.popup2').text('');
+      setTimeout(function() {
+        $('.popup1').text('');
+      }, 1500);
+    }
+  });
 }); //End of click
 
 // Reset Button
